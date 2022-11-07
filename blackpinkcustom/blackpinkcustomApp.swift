62ec3157078be4509ca2b5ca82758123d025e18b
Pscabby <pscabby0506@gmail.com>
2022-11-07 22:56:47 +0800

    Initial Commit

---
+++ blackpinkcustom/blackpinkcustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackpinkcustomApp.swift
+//  blackpinkcustom
+//
+//  Created by Abby on 2022/11/7.
+//
+
+import SwiftUI
+
+@main
+struct blackpinkcustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
